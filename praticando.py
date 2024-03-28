ba0fd207877dd84921e1f9f922a126580abbbd77
PJ - Data Analytics 2024.1 <3668822024.1@senacrjedu.local>
2024-03-28 09:06:14 -0300

    subindo trabalho

---
+++ praticando.py
@@ -0,0 +1,15 @@
+pergunta_usuario = input('Faça a sua pergunta: ')
+if pergunta_usuario == 'QUAL É O NOME COMPLETO DA PROTAGONISTA DO EPISÓDIO?' :
+    print('Joan Taif')
+elif pergunta_usuario == 'QUEM DIRIGE A VIDA DE JOAN EM TEMPO REAL PARA UMA SÉRIE DE TELEVISÃO?':
+    print ('Salma Hayek')
+elif pergunta_usuario == 'QUAL É O NOME DO SERVIÇO DE STREAMING FICTÍCIO QUE PARODIA A NETFLIX NO EPISÓDIO?':
+    print('Streamberry')
+elif pergunta_usuario == 'COMO JOAN DESCOBRE A EXISTÊNCIA DA SÉRIE SOBRE SUA VIDA?':
+    print('Escolhe a serie por que viu seu nome na serie')
+elif pergunta_usuario == 'QUAL É A REAÇÃO INICIAL DE JOAN AO DESCOBRIR A SÉRIE E O QUE ELA FAZ EM RESPOSTA?':
+    print( 'Fica desesperada e tenta desligar')
+elif pergunta_usuario == 'QUAIS SÃO OS TEMAS PRINCIPAIS EXPLORADOS NESTE EPISÓDIO DE BLACK MIRROR?':
+    print( 'Inteligência artificial, direitos de imagem, desatenção com contratos')
+elif pergunta_usuario == 'O EPISÓDIO TERMINA DE MANEIRA TÍPICA PARA A SÉRIE BLACK MIRROR? EXPLIQUE.':
+    print('Não. Porque teve final feliz!')
